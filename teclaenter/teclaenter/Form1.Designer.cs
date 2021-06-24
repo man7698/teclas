bce25cffae0966957f391cdf969dd396ef4ff462
man7698 <matheusalmeida7698@gmail.com>
2021-06-24 01:36:04 -0300

    Add files via upload

---
+++ teclaenter/teclaenter/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace teclaenter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtnome1 = new System.Windows.Forms.TextBox();
+            this.txtseparar = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(51, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(134, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Digite um nome";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(51, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(134, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Digite um nome";
+            // 
+            // txtnome1
+            // 
+            this.txtnome1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtnome1.Location = new System.Drawing.Point(55, 43);
+            this.txtnome1.Name = "txtnome1";
+            this.txtnome1.Size = new System.Drawing.Size(135, 26);
+            this.txtnome1.TabIndex = 2;
+            this.txtnome1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtnome1_KeyDown);
+            // 
+            // txtseparar
+            // 
+            this.txtseparar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtseparar.Location = new System.Drawing.Point(55, 116);
+            this.txtseparar.Name = "txtseparar";
+            this.txtseparar.Size = new System.Drawing.Size(135, 26);
+            this.txtseparar.TabIndex = 3;
+            this.txtseparar.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtseparar_KeyDown);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(260, 72);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(99, 41);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Separar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(438, 190);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtseparar);
+            this.Controls.Add(this.txtnome1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtnome1;
+        private System.Windows.Forms.TextBox txtseparar;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ teclaenter/teclaenter/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace teclaenter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (txtnome1.Text.Length == 0)
+            {
+                MessageBox.Show("Você precisa digitar um nome !!");
+            }
+            else
+            {
+                string nome = txtnome1.Text;
+                txtseparar.Text = nome.Substring(3, 2);//primeira posição é a posição que comeca a contar e segunda posição é quantas vezes conta
+            }
+
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            string texto = "Deseja realmente sair";
+            string titulo = "finalizar aplicação";
+            if(MessageBox.Show(texto,titulo,MessageBoxButtons.YesNo,MessageBoxIcon.Question,MessageBoxDefaultButton.Button2)==DialogResult.No)
+            {
+                e.Cancel = true;
+            }
+        }
+
+        
+
+        private void txtnome1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                SendKeys.Send("{TAB}");
+            }
+            else if (e.KeyCode == Keys.Escape)
+            {
+                this.Close();
+            }
+        }
+
+        private void txtseparar_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                SendKeys.Send("{TAB}");
+            }
+            else if (e.KeyCode == Keys.Escape)
+            {
+                this.Close();
+            }
+        }
+    }
+}
